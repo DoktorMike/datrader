acf714023eb0848288bfb21b0acede27c26d1939
Michael <micke.green@gmail.com>
2016-05-01 11:42:35 +0200

    updated interactive

--- interactive.R
+++ interactive.R
@@ -1,7 +1,13 @@
 library(datrader)
+library(quantmod)
+library(ggplot2)
 
 mypath<-'/home/michael/Dropbox/Development/trading'
 
 mylist<- loadExistingInstruments(mypath)
 
-lapply(names(mylist), function(x) {readline(paste0("Press enter to see: ", x)); chartSeries(mylist[[x]])})
+lapply(names(mylist), function(x) {a<-readline(paste0("Press enter to see: ", x, " and q to stop: ")); if(a=="q") stop("Bye") else chartSeries(mylist[[x]])})
+
+a<-getSymbols("VOW.DE", auto.assign = F)
+a<-mylist[[2]]
+b<-adjustOHLC(a)
