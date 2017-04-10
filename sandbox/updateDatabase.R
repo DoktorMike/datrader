3f763b27a5fcd918dcf73db7d8e037d5df9cda27
Michael <micke.green@gmail.com>
2017-04-10 15:13:14 +0200

    Added more symbols.

--- sandbox/updateDatabase.R
+++ sandbox/updateDatabase.R
@@ -7,9 +7,11 @@ omxs30Symbols<-c('ABB.ST', 'ALFA.ST', 'ASSA-B.ST', 'AZN.ST', 'ATCO-A.ST',
                  'INVE-B.ST', 'LUPE.ST', 'MTG-B.ST', 'NDA-SEK.ST', 'SAND.ST',
                  'SCA-B.ST', 'SCV-B.ST', 'SEB-A.ST', 'SECU-B.ST', 'SKA-B.ST', 'SKF-B.ST',
                  'SSAB-A.ST', 'SHB-A.ST', 'SWED-A.ST', 'SWMA.ST', 'TEL2-B.ST', 'TLSN.ST', 'VOLV-B.ST')
+nasdaqSymbols<-c('TSLA', 'MSFT', 'NVDA', 'AMD', 'INTC', 'FB', 'GOOG', 'AMZN')
 
 # Should only run once!
 downloadInstruments(omxs30Symbols, mypath, '2000-01-01')
+downloadInstruments(nasdaqSymbols, mypath, '2000-01-01')
 # updateInstruments(mypath)
 # updateInstruments(mypath, startDate = Sys.Date() - 90)
 
