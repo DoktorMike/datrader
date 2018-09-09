b9a6542f8363fc3261f1655d33c0ce496d2cf5bf
Michael Green <micke.green@gmail.com>
2018-09-09 13:00:55 +0200

    Added data infrastructure.

---
+++ datrader/tests/testthat/test_fetch.R
@@ -0,0 +1,8 @@
+context("Data fetching")
+library(datrader)
+
+test_that("str_length is number of characters", {
+  expect_equal(str_length("a"), 1)
+  expect_equal(str_length("ab"), 2)
+  expect_equal(str_length("abc"), 3)
+})
