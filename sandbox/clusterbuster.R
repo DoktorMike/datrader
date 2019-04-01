d6aa8ca4b475b887123a6483042643fec5540b6f
Michael Green <micke.green@gmail.com>
2019-04-01 11:41:59 +0200

    Small examplt.

--- sandbox/clusterbuster.R
+++ sandbox/clusterbuster.R
@@ -2,3 +2,12 @@ myres <- tibble(H1=clusterStocks(mylist, k=10, horizon=10),
                 H2=clusterStocks(mylist, k=10, horizon=11),
                 H3=clusterStocks(mylist, k=10, horizon=12)
                 )
+
+
+library(dplyr)
+library(ggplot2)
+
+tibble(x=rnorm(10)) %>% mutate(y=x+5) %>% ggplot(aes(x=x, y=y)) + geom_point()
+
+
+
